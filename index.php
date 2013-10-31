cd8dddd7f57e5cbae0a6debdcf67066b7fffcb2f
andersonstano <andersonstano@gmail.com>
2013-10-31 11:44:15 -0200

    Commit #2

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    include_once("start.html");
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8" />
+        <title></title>
+    </head>
+    <body>
+        
+    </body>
+</html>
